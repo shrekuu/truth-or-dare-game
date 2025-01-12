0dfc8e7521fc6cb8150bc3bb4104af7e03feab9e
Bond <shrekuu@gmail.com>
2025-01-12 17:18:18 +0800

    add shadcn; flip the card once it is clicked; read the text aloud if the device supports it

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,61 @@
+import { useState } from "react";
 import "./App.css";
 import { emoticons, tasks } from "./data";
+import { AlertDialog, AlertDialogAction, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "@/components/ui/alert-dialog";
 
-export default function App() {
+type TTask = string;
+
+const Item = ({ task }: { task: TTask }) => {
+  const [viewed, setViewed] = useState(false);
+
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="grid grid-cols-3 gap-4 p-4">
-      {tasks.map((task, index) => (
+    <div className="border border-neutral-300 aspect-square flex justify-center items-center text-7xl">
+      {viewed ? (
+        <div className="grayscale">🫥</div>
+      ) : (
         <div
-          key={index}
-          className="border p-4 text-center rounded"
           onClick={() => {
-            alert(emoticons[Math.floor(Math.random() * emoticons.length)] + " " + task);
+            setOpen(true);
+            setViewed(true);
+
+            // read the task
+            const utterance = new SpeechSynthesisUtterance(task);
+            speechSynthesis.speak(utterance);
           }}
         >
-          <div>{emoticons[Math.floor(Math.random() * emoticons.length)]}</div>
+          {emoticons[Math.floor(Math.random() * emoticons.length)]}
         </div>
-      ))}
+      )}
+
+      <AlertDialog open={open} onOpenChange={setOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle className="text-3xl">{task}</AlertDialogTitle>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogAction>OK</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  );
+};
+
+export default function App() {
+  const shuffledTasks = tasks.sort(() => Math.random() - 0.5);
+
+  return (
+    <div>
+      <header className="p-4 bg-gray-200">
+        <h1 className="text-3xl">盲盒大冒险</h1>
+      </header>
+      <main className="grid grid-cols-5 gap-2 p-2">
+        {shuffledTasks.map((task, index) => (
+          <Item key={index} task={task} />
+        ))}
+      </main>
     </div>
   );
 }
